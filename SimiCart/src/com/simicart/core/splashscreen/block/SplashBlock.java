fa063e4f91377ce5761af9e9c728178e497a0594
truongtechno <truongnhthtb@gmail.com>
2016-03-17 09:24:35 +0700

    Fix lỗi nếu khách upload ảnh Spash quá nặng thì app sẽ crash. Trong trường hợp này mình không cần bắt khách phải up đúng kich thước ảnh, mình sẽ resize kích thước ảnh trong SplashBlock luôn

--- SimiCart/src/com/simicart/core/splashscreen/block/SplashBlock.java
+++ SimiCart/src/com/simicart/core/splashscreen/block/SplashBlock.java
@@ -2,6 +2,9 @@ package com.simicart.core.splashscreen.block;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
@@ -51,10 +54,55 @@ public class SplashBlock {
 		ImageView img_logo = (ImageView) rootView.findViewById(Rconfig
 				.getInstance().getId(mContext, "core_splash_screen_img_logo",
 						"id"));
-		img_logo.setImageResource(Rconfig.getInstance().getId(mContext,
-				"default_splash_screen", "drawable"));
+		/*img_logo.setImageResource(Rconfig.getInstance().getId(mContext,
+				"default_splash_screen", "drawable"));*/
+		img_logo.setImageBitmap(decodeSampledBitmapFromResource(
+				mContext.getResources(),
+				Rconfig.getInstance().drawable("default_splash_screen"),
+				700, 700));
 		img_logo.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 
+	}
+
+	public Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+			int reqWidth, int reqHeight) {
+
+		// First decode with inJustDecodeBounds=true to check dimensions
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeResource(res, resId, options);
+
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, reqWidth,
+				reqHeight);
+
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeResource(res, resId, options);
+	}
+
+	public int calculateInSampleSize(BitmapFactory.Options options,
+			int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			final int halfHeight = height / 2;
+			final int halfWidth = width / 2;
+
+			// Calculate the largest inSampleSize value that is a power of 2 and
+			// keeps both
+			// height and width larger than the requested height and width.
+			while ((halfHeight / inSampleSize) > reqHeight
+					&& (halfWidth / inSampleSize) > reqWidth) {
+				inSampleSize *= 2;
+			}
+		}
+
+		return inSampleSize;
 	}
 
 }
