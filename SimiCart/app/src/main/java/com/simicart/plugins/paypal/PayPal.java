b1a046b779c5cf2b653bc5bacb71baf7b44be7d2
duclong <duclong.bui@trueplus.vn>
2016-03-24 09:27:29 +0700

    Add plugin Paypal mobile

---
+++ SimiCart/app/src/main/java/com/simicart/plugins/paypal/PayPal.java
@@ -0,0 +1,93 @@
+package com.simicart.plugins.paypal;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.simicart.MainActivity;
+import com.simicart.core.base.manager.SimiManager;
+import com.simicart.core.checkout.entity.PaymentMethod;
+import com.simicart.core.common.Utils;
+import com.simicart.core.event.checkout.CheckoutData;
+
+public class PayPal {
+	Context context;
+
+	// check don't have client ID -> refresh list payment
+	public PayPal(Context context, String method,
+			ArrayList<PaymentMethod> paymentList) {
+		this.context = context;
+		if (method.equals("refreshList")) {
+			this.refreshList(paymentList);
+		}
+	}
+
+	public PayPal(String method, CheckoutData checkoutData) {
+		this.context = MainActivity.context;
+		Log.e("Paypal", "Paypal" + method);
+		if (method.equals("callPayPalServer")) {
+			this.callPayPalServer(checkoutData.getPaymentMethod(),
+					checkoutData.getInvoice_number(),
+					checkoutData.getTotal_price());
+		}
+	}
+
+	public void refreshList(ArrayList<PaymentMethod> paymentList) {
+		int i = 0;
+		int position = -1;
+
+		for (PaymentMethod payment : paymentList) {
+			if (payment.getPayment_method().equals("PAYPAL_MOBILE")) {
+				if (payment.getData("client_id").equals("null")
+						|| payment.getData("client_id") == null
+						|| payment.getData("client_id").equals("")) {
+					position = i;
+					break;
+				}
+			}
+			i++;
+		}
+		if (position != -1) {
+			paymentList.remove(position);
+		}
+	}
+
+	public void callPayPalServer(PaymentMethod paymentMethod,
+			String invoice_number, String total_price) {
+		if (paymentMethod.getPayment_method().equals("PAYPAL_MOBILE")) {
+			Intent intent = new Intent(this.context, PaypalSimicart.class);
+
+			String client_id = paymentMethod.getData("client_id");
+			if (!Utils.validateString(client_id)) {
+				SimiManager
+						.getIntance()
+						.showNotify(
+								"The requested Payment Method is not available. Don't have a Client_ID");
+				SimiManager.getIntance().backToHomeFragment();
+				return;
+			}
+			intent.putExtra("EXTRA_CLIENT_ID", client_id);
+			Log.e("PayPal CLIENT_ID ", client_id);
+
+			String is_sandbox = paymentMethod.getData("is_sandbox");
+			intent.putExtra("EXTRA_SANDBOX", is_sandbox);
+			Log.e("PayPal IS_SANDBOX ", is_sandbox);
+
+			intent.putExtra("EXTRA_PRICE", total_price);
+			Log.e("PayPal EXTRA_PRICE", total_price);
+
+			String bnCode = paymentMethod.getData("bncode");
+			intent.putExtra("EXTRA_BNCODE", bnCode);
+			Log.e("PayPal BNCODE ", bnCode);
+
+			intent.putExtra("EXTRA_INVOICE_NUMBER", invoice_number);
+			Log.e("PayPal INVOICE NUMBER ", invoice_number);
+			this.context.startActivity(intent);
+		} else {
+			return;
+		}
+
+	}
+}
